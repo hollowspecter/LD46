47e5fa7ca018557be3f72b14aaf656c9d0c5fda6
Julian Viezens <julivizi@yahoo.de>
2020-04-18 20:45:40 +0200

    Fixed DangerCannon and DangerRocketLauncherDude

---
+++ Assets/Scripts/Dangers/Danger_Cannon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Danger_Cannon : Danger
+{
+    public GameObject ammoPrefab;
+    public Transform spawnPosition;
+    public ParticleSystem pSystem;
+    public float cannonPower = 10.0f;
+
+    protected override void StartDanger()
+    {
+        Rigidbody rigid = GameObject.Instantiate(ammoPrefab, spawnPosition.position, spawnPosition.rotation).GetComponent<Rigidbody>();
+        rigid.AddForce(spawnPosition.forward * cannonPower, ForceMode.Impulse);
+        if(pSystem)
+            pSystem.Play();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(spawnPosition.position, spawnPosition.forward * 50.0f);
+    }
+}
---
+++ Assets/Scripts/Dangers/ExplosiveDanger.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveDanger : MonoBehaviour
+{
+    bool hasExploded = false;
+    public float explosionPower = 200f;
+    public float explosionRadius = 10.0f;
+    public ParticleSystem explosionParticles;
+    Rigidbody rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (hasExploded) return;
+        Debug.Log("Explode " + collision.gameObject.name);
+        Explode();
+    }
+
+    private void Explode()
+    {
+        hasExploded = true; 
+        Collider[] colliders = Physics.OverlapSphere(rigid.position, explosionRadius);
+        HashSet<Rigidbody> rbs = new HashSet<Rigidbody>();
+        foreach (Collider hit in colliders)
+        {
+            if(hit.gameObject != gameObject)
+            {
+                if (hit.gameObject.tag == "Baby")
+                    hit.GetComponentInParent<Baby>().Die();
+                Rigidbody rb = (hit.GetComponentInParent<Rigidbody>());
+                if(rb!= null)
+                {
+                    rbs.Add(rb);
+                }
+            }
+        }
+        foreach(Rigidbody rb in rbs)
+        {
+            rb.AddExplosionForce(explosionPower, rigid.position, explosionRadius, 0.5f, ForceMode.Impulse);
+        }
+        if(explosionParticles != null)
+        {
+            explosionParticles.Play();
+        }
+        rigid.isKinematic = true;
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
