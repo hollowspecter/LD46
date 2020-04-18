db461d4037fddf07c3d96bc99c2b37d6e8b6068d
Julian Viezens <julivizi@yahoo.de>
2020-04-18 15:48:26 +0200

    Removed RoadKit
    Added RacingKit
    Axe PrefabUpdate

---
+++ Assets/Scripts/Dangers/Danger_Axe.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Danger_Axe : Danger
+{
+    public override void Pause()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Unpause()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void PausedUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void UnpausedUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
