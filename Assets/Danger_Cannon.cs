96b81fa20cadb96ff0f4c465838c9370f6148c09
Julian Viezens <julivizi@yahoo.de>
2020-04-18 18:45:57 +0200

    Added Danger_Cannon

---
+++ Assets/Danger_Cannon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Danger_Cannon : Danger
+{
+    public Rigidbody cannonBallRigid;
+    public Transform barrel;
+    public ParticleSystem pSystem;
+    public float cannonPower = 10.0f;
+
+    protected override void StartDanger()
+    {
+        cannonBallRigid.gameObject.SetActive(true);
+        cannonBallRigid.isKinematic = false;
+        cannonBallRigid.AddForce(-barrel.forward * cannonPower, ForceMode.Impulse);
+        pSystem.Play();
+    }
+}
