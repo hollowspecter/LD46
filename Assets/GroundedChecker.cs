a202979394f85c817815c86d0590cb766781ce14
Julian Viezens <julivizi@yahoo.de>
2020-04-18 23:19:33 +0200

    Cards now have a grounded check and constant acceleration thingy

---
+++ Assets/GroundedChecker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedChecker : MonoBehaviour
+{
+    public bool isGrounded = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.root != transform.root)
+            isGrounded = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root != transform.root)
+            isGrounded = false;
+    }
+}
