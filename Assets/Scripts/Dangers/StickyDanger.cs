5a1df22b5d6cd2a9208d52356bc1af0e7653ae03
Vivien Baguio <baguiov@posteo.net>
2020-04-19 14:41:25 +0200

    add sound to danger axe

--- Assets/Scripts/Dangers/StickyDanger.cs
+++ Assets/Scripts/Dangers/StickyDanger.cs
@@ -5,22 +5,26 @@ using UnityEngine.Events;
 
 public class StickyDanger : MonoBehaviour
 {
-    public Rigidbody ownRigid;
-    bool isSticking = false;
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (isSticking) return;
-        if(collision.rigidbody != null)
-        {
-            Stick(collision.rigidbody);
-        }
-    }
-    public void Stick(Rigidbody r)
-    {
-        isSticking = true;
-        ownRigid.angularVelocity = Vector3.zero;
-        ownRigid.velocity = Vector3.zero;
-        FixedJoint f = r.gameObject.AddComponent<FixedJoint>();
-        f.connectedBody = ownRigid;
-    }
+	public Rigidbody ownRigid;
+	bool isSticking = false;
+	public UnityEvent onStick;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (isSticking)
+			return;
+		if (collision.rigidbody != null)
+		{
+			Stick(collision.rigidbody);
+		}
+	}
+	public void Stick(Rigidbody r)
+	{
+		isSticking = true;
+		ownRigid.angularVelocity = Vector3.zero;
+		ownRigid.velocity = Vector3.zero;
+		FixedJoint f = r.gameObject.AddComponent<FixedJoint>();
+		f.connectedBody = ownRigid;
+		onStick?.Invoke();
+	}
 }
