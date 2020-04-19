6b976e6c8432fa938eff297f162e5791df1935e9
Vivien Baguio <baguiov@posteo.net>
2020-04-19 21:43:15 +0200

    add radio that can break

---
+++ Assets/Scripts/Radio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Radio : MonoBehaviour
+{
+	public float breakForce = 5f;
+
+	public UnityEvent onBreak;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log($"Collision! {collision.impulse.magnitude}");
+		if (collision.impulse.magnitude > breakForce)
+		{
+			onBreak?.Invoke();
+		}
+	}
+}
