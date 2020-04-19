6fc5cde32989bb3c00451647851d73a6bfe3f3ba
Vivien Baguio <baguiov@posteo.net>
2020-04-19 16:46:47 +0200

    add sounds to sea level

--- Assets/Scripts/Dangers/Danger_Cannon.cs
+++ Assets/Scripts/Dangers/Danger_Cannon.cs
@@ -1,28 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Danger_Cannon : Danger
 {
-    public GameObject ammoPrefab;
-    public Transform spawnPosition;
-    public ParticleSystem pSystem;
-    public float cannonPower = 10.0f;
+	public GameObject ammoPrefab;
+	public Transform spawnPosition;
+	public ParticleSystem pSystem;
+	public float cannonPower = 10.0f;
+	public UnityEvent onShot;
 
-    protected override void StartDanger()
-    {
-        Rigidbody rigid = GameObject.Instantiate(ammoPrefab, spawnPosition.position, spawnPosition.rotation).GetComponent<Rigidbody>();
-        rigid.AddForce(spawnPosition.forward * cannonPower, ForceMode.Impulse);
-        if(pSystem)
-            pSystem.Play();
-    }
+	protected override void StartDanger()
+	{
+		Rigidbody rigid = GameObject.Instantiate(ammoPrefab, spawnPosition.position, spawnPosition.rotation).GetComponent<Rigidbody>();
+		rigid.AddForce(spawnPosition.forward * cannonPower, ForceMode.Impulse);
+		if (pSystem)
+			pSystem.Play();
+		onShot?.Invoke();
+	}
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(spawnPosition.position, spawnPosition.forward * 150.0f);
-        Vector3 forwardFlat = spawnPosition.forward;
-        forwardFlat.y = 0.0f;
-        Gizmos.DrawRay(transform.position, forwardFlat * 150.0f);
-    }
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay(spawnPosition.position, spawnPosition.forward * 150.0f);
+		Vector3 forwardFlat = spawnPosition.forward;
+		forwardFlat.y = 0.0f;
+		Gizmos.DrawRay(transform.position, forwardFlat * 150.0f);
+	}
 }
